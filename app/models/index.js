c44b0d8ac1d19715f5b991f6d08e948e58b798b2
kburre <k.burre@eagles.oc.edu>
2023-07-03 05:01:20 -0500

    Homapage api added

--- app/models/index.js
+++ app/models/index.js
@@ -16,6 +16,10 @@ db.sequelize = sequelize;
 
 db.session = require("./session.model.js")(sequelize, Sequelize);
 db.user = require("./user.model.js")(sequelize, Sequelize);
+db.company = require("./company.model.js")(sequelize, Sequelize);
+db.customer = require("./customers.model.js")(sequelize, Sequelize);
+db.service_ticket = require("./service_ticket.model.js")(sequelize, Sequelize);
+
 
 // foreign key for session
 db.user.hasMany(
@@ -28,4 +32,60 @@ db.session.belongsTo(
   { as: "user" },
   { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
 );
+
+
+// foreign key for company
+db.company.hasMany(
+  db.user,
+  { as: "user" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+db.user.belongsTo(
+  db.company,
+  { as: "company" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+
+
+
+// foreign key for customer
+db.company.hasMany(
+  db.customer,
+  { as: "customer" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+db.customer.belongsTo(
+  db.company,
+  { as: "company" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+
+
+// foreign key for service_ticket
+db.service_ticket.belongsTo(
+  db.customer,
+  { as: "delivery_customer" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+
+db.service_ticket.belongsTo(
+  db.customer,
+  { as: "pickup_customer" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+
+
+db.service_ticket.belongsTo(
+  db.user,
+  { as: "creator" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+
+db.service_ticket.belongsTo(
+  db.user,
+  { as: "assigned_to" },
+  { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
+);
+
+
 module.exports = db;
